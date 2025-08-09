5c05c0190550980462d093d9c54cfb312a4a14d8
IshikawaUta <jayaj7129@gmail.com>
2025-08-09 14:40:30 +0700

    portofolio milligram css

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,1 +1,22 @@
-// file customElements.js
+// JavaScript untuk tombol "Scroll Up"
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollUpButton = document.getElementById('scrollUp');
+
+    // Tampilkan atau sembunyikan tombol saat menggulir
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) { // Munculkan tombol setelah menggulir 300px
+            scrollUpButton.classList.add('show');
+        } else {
+            scrollUpButton.classList.remove('show');
+        }
+    });
+
+    // Gulir ke atas saat tombol diklik
+    scrollUpButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
